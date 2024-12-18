c9d9b45f509c1eb6df5bb908d33c627fd8d6520e
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2024-12-18 12:16:06 +0100

    poprawa

--- ptk_odniesienia.m
+++ ptk_odniesienia.m
@@ -6,12 +6,31 @@ punkty_n = size(non_dominated,1);
 ranking = zeros([punkty_n,1]);
 for i  = 1:punkty_n
     rank = 0;
+    objSum = 0;
     for ia1 = 1:size(A1,1)
-        rank = rank + norm(non_dominated(i,:) - A1(ia1,:)) * abs(prod(non_dominated(i,:) - A1(ia1,:)));
+        for ia2 = 1:size(A2,1)
+            inside = 1;
+            for n  = 1: size(A1,2)
+                if max([A1(ia1,n),A2(ia2,n)]) < non_dominated(i,n)
+                    inside = 0;
+                    break
+                end
+                if min([A1(ia1,n),A2(ia2,n)]) > non_dominated(i,n)
+                    inside = 0;
+                    break
+                end
+            end
+            if(inside)
+                obj = abs(prod(A1(ia1,:) - A2(ia2,:)));
+                aspiracji = norm(non_dominated(i,:) - A1(ia1,:));
+                status_quo = norm(non_dominated(i,:) - A2(ia2,:));
+                rank = rank + obj*status_quo/(status_quo+aspiracji);
+                objSum  = objSum + obj;
+            end
+
+        end
     end
-    for ia2 = 1:size(A2,1)
-        rank = rank - norm(non_dominated(i,:) - A2(ia2,:)) * abs(prod(non_dominated(i,:) - A2(ia2,:)));
-    end
-    ranking(i) = rank;
+
+    ranking(i) = rank/objSum    ;
 end
 end
